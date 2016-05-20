d0951fd56c0b445de9300bdc6936022b67da7682
bijanbina <bijanbina@gmail.com>
2016-05-20 13:09:25 +0430

    add Shatter Me

---
+++ ShatterMe/ShatterMe.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-05-20T11:16:56
+#
+#-------------------------------------------------
+
+QT       += core \
+            serialport
+
+QT       -= gui
+
+TARGET = ShatterMe
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    chaper.cpp
+
+HEADERS += \
+    chaper.h
---
+++ ShatterMe/chaper.h
@@ -0,0 +1,34 @@
+#ifndef CHAPER_H
+#define CHAPER_H
+
+#include <QObject>
+#include <QSerialPort>
+#include <QDebug>
+
+#define PORT_NAME "ttyUSB0"
+#define BAUD_RATE QSerialPort::Baud9600
+#define DATA_BITS QSerialPort::Data8
+#define PARITY    QSerialPort::NoParity
+#define STOP_BITS QSerialPort::OneStop
+#define FLOW_CONT QSerialPort::NoFlowControl
+
+class chaper : public QObject
+{
+    Q_OBJECT
+public:
+    explicit chaper(QObject *parent = 0);
+    ~chaper();
+
+signals:
+
+public slots:
+
+private slots:
+    void updateData();
+
+private:
+    void openSerialPort();
+    QSerialPort *channel;
+};
+
+#endif // CHAPER_H
