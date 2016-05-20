006eeb5f1858c70f18d35bd29e74633da0d9966b
bijanbina <bijanbina@gmail.com>
2016-05-20 14:58:48 +0430

    add SNMP

--- ShatterMe/chaper.cpp
+++ ShatterMe/chaper.cpp
@@ -5,6 +5,7 @@ chaper::chaper(QObject *parent) : QObject(parent)
     channel = new QSerialPort(this);
     openSerialPort();
     connect(channel,SIGNAL(readyRead()),this,SLOT(updateData()));
+    channel->write("12\n");
 }
 
 void chaper::openSerialPort()
@@ -35,6 +36,29 @@ void chaper::updateData()
 {
     QByteArray data;
     data = channel->read(1);
+    if (*data.data() == '\n')
+    {
+        bool ok;
+        int temp = QString(buffer).toInt(&ok);
+        if (ok)
+        {
+            qInfo() << QString("set temp to %1").arg(temp);
+            QString command = "snmpset -v2c -c tutset localhost NET-SNMP-TUTORIAL-MIB::nstAgentModuleObject.0 = ";
+            command.append(buffer);
+            //qInfo() << QString("run %1").arg(command);
+            getStrCommand(command);
+        }
+        else
+        {
+            qCritical() << "Error : " << "Entered value is not a integer";
+        }
+        buffer = "";
+    }
+    else
+    {
+        buffer.append(data);
+        qInfo() << QString("receive %1").arg(data.data());
+    }
 }
 
 chaper::~chaper()
@@ -43,4 +67,33 @@ chaper::~chaper()
     {
         channel->close();
     }
+}
+
+
+
+QString chaper::getStrCommand(QString command)
+{
+    FILE *fp;
+    QString returnData;
+
+    char path[1035];
+
+    /* Open the command for reading. */
+    fp = popen(command.toStdString().c_str(), "r");
+
+    if (fp == NULL) {
+      printf("Failed to run command\n" );
+      return returnData;
+    }
+
+    /* Read the output a line at a time - output it. */
+    while (fgets(path, sizeof(path)-1, fp) != NULL) {
+      returnData = QString(path);
+    }
+
+    returnData.remove('\n');
+
+    /* close */
+    pclose(fp);
+    return returnData;
 }
