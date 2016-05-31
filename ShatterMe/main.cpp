be00b66eef5322bae11e508493a76d4cdb280f03
Bijan Binaee <bijanbina@gmail.com>
2016-05-31 23:18:21 +0430

    fix Mostafa bug

--- ShatterMe/main.cpp
+++ ShatterMe/main.cpp
@@ -7,7 +7,19 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     qInfo() << "Service Started";
     chapar *sender = new chapar();
-    /*while(true)
+    
+    /*char send_command[100];
+    send_command[0] = 0x52;
+    send_command[1] = 0x00;
+    send_command[2] = 0x1f;
+    send_command[3] = 0;
+    send_command[4] = 0;
+    send_command[5] = 0;
+    send_command[6] = 0;
+    send_command[7] = MakeCRC(send_command);
+    shatter_debug_hex("crc:\t", send_command, PACKET_LEN);
+    
+    while(true)
     {
         QString command = "/usr/local/bin/snmpset -v2c -c tutset localhost NET-SNMP-TUTORIAL-MIB::nstAgentModuleObject.0 = ";
         command.append(QString("%1").arg(12));
