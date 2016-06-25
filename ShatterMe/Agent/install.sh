499e8ccb1d42886dccda3afc8f00154730695a3a
Bijan Binaee <bijanbina@gmail.com>
2016-06-25 09:11:31 +0430

    add start script

--- ShatterMe/Agent/install.sh
+++ ShatterMe/Agent/install.sh
@@ -1,4 +1,6 @@
 #/bin/sh
-cp ./NET-SNMP-TUTORIAL-MIB.txt /usr/local/share/snmp/mibs/
-cp ./snmpd.conf /usr/local/etc/snmp/
-
+cp	./NET-SNMP-TUTORIAL-MIB.txt /usr/local/share/snmp/mibs/
+cp	./snmpd.conf /usr/local/etc/snmp/
+cp	./shatterme.service /etc/systemd/system/
+cp	./start /usr/bin
+systemctl enable shatterme
