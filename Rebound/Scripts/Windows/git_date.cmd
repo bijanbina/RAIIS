8f3772d5540dd1c1b3b8dc7f4918d0868105723f
bijanbina <bijanbina@gmail.com>
2022-09-01 10:39:39 +0430

    Rebound-Windwos: Disable Automatic Windows Time Sync [Ehsan]

--- Rebound/Scripts/Windows/git_date.cmd
+++ Rebound/Scripts/Windows/git_date.cmd
@@ -7,13 +7,15 @@ set mon=%mydate%
 set mydate=%date:~10,4%
 set yer=%mydate%
 
-net stop w32time 
+::net stop w32time 
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters /f /v Type /t REG_SZ /d NoSync
 
 :loop
 SET /P D=Enter day:
 if %D%==q (
-	net start w32time
+	::net start w32time
 	goto end
+	reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters /f /v Type /t REG_SZ /d NTP
 	)
 if %D% LEQ %day% (
 	date %mon%-%D%-%yer%
