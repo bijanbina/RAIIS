903e957c5ce0b78473750bb553638e9e8ea1665f
bijanbina <bijanbina@gmail.com>
2024-01-06 19:34:54 +0100

    Reabound-Windows: Create Helper Functions for addHwnd

--- Rebound/Sources/re_callback_w.cpp
+++ Rebound/Sources/re_callback_w.cpp
@@ -1,15 +1,15 @@
 #include "re_admin_win.h"
 #include "re_window_w.h"
+#include "mm_api.h"
 
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
 {
-    char buffer[128];
-    ReWindowW *thread_w = (ReWindowW *)lParam;
-    int written = GetWindowTextA(hwnd, buffer, 128);
-    if( written && strlen(buffer)!=0 )
+    ReWindowW *re_win = (ReWindowW *)lParam;
+    if( re_isNormalHwnd(hwnd) )
     {
-        re_AddHwnd(hwnd, thread_w);
+        re_win->addHwnd(hwnd);
     }
+
     return TRUE;
 }
 
@@ -28,4 +28,74 @@ BOOL CALLBACK EnumChildProc(HWND hwnd, LPARAM lParam)
     {
         return TRUE;
     }
+}
+
+int re_cleanRemoteId(QString title)
+{
+    QStringList split = title.split('-');
+    QStringList id_split = split[0].split(" ");
+
+    return id_split[0].toInt();
+}
+
+int re_isNormalHwnd(HWND hwnd)
+{
+    char buffer[128];
+    int written = GetWindowTextA(hwnd, buffer, 128);
+    if( written==0 || strlen(buffer)==0 )
+    {
+        return 0;
+    }
+
+    if( IsWindowVisible(hwnd)==0 )
+    {
+        return 0;
+    }
+
+    int cloaked;
+    DwmGetWindowAttribute(hwnd, DWMWA_CLOAKED, &cloaked, 4);
+    if( cloaked )
+    {
+        return 0;
+    }
+
+    HWND shell_window = GetShellWindow();
+    if( hwnd==shell_window )
+    {
+        return 0;
+    }
+
+    RECT rc;
+    GetWindowRect(hwnd, &rc);
+    int width = rc.right - rc.left;
+    int height = rc.bottom - rc.top;
+    int opacity = re_getWindowOpacity(hwnd);
+
+    if( width  <MM_MINWIN_WIDTH ||
+        height <MM_MINWIN_HEIGHT ||
+        opacity<MM_MINWIN_OPACITY ) //|| (rc.bottom<0)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+ReWindow re_readWindow(HWND hwnd)
+{
+    ReWindow win;
+    char buffer[128];
+
+    int success = GetWindowTextA(hwnd, buffer, 128); //get title
+    if ( success==0 )
+    {
+        qDebug() << hwnd << "Failed to GetWindowTextA";
+    }
+
+    win.hWnd = hwnd;
+    win.title = buffer;
+    win.pname = mm_getPName(mm_getPid(hwnd));
+    win.verify = 1; //always new windows are verified
+
+    return win;
 }
--- Rebound/Sources/re_callback_w.h
+++ Rebound/Sources/re_callback_w.h
@@ -8,8 +8,12 @@
 #include <tchar.h> // to get file path
 #include <psapi.h> // For access to GetModuleFileNameEx
 #include "backend.h"
+#include "re_commands.h"
 
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam);
 BOOL CALLBACK EnumChildProc  (HWND hwnd, LPARAM lParam);
+ReWindow re_readWindow(HWND hwnd);
+int      re_cleanRemoteId(QString title);
+int      re_isNormalHwnd(HWND hwnd);
 
 #endif // RE_CALLBACK_W_H
