fe184d584f22241ce0e0f2c29933956e8ffee2e6
bijanbina <bijanbina@gmail.com>
2024-02-17 15:33:35 +0330

    Rebound-Windows: Remove Sign Requirement for Firefox Addon[E&B]

--- Rebound/Scripts/Firefox/init.lua
+++ Rebound/Scripts/Firefox/init.lua
@@ -1,3 +1,5 @@
+-- This file is executed when rebound starts
+
 require("re_system")
 require("re_string")
 require("getPrefPath")
@@ -14,6 +16,13 @@ UBI_PATTERN = 'app.update.background.interval'
 UPD_PATTERN = 'app.update.interval'
 URL_PATTERN = 'browser.safebrowsing.provider.google.updateURL'
 
+RTC_PATTERN = '%"privacy.webrtc.hideGlobalIndicator'
+NAV_PATTERN = '%"media.navigator.permission.disabled'
+PLY_PATTERN = '%"media.block%-autoplay%-until%-in%-foreground' -- youtube autoplay
+RST_PATTERN = '%"extensions.webextensions.restrictedDomains' -- remove addon restriction
+FNG_PATTERN = '%"privacy.resistFingerprinting.block_mozAddonManager' -- remove addon restriction
+SGN_PATTERN = '%"xpinstall.signatures.required' -- remove addon restriction
+
 CRM_LINE = 'user_pref("devtools.chrome.enabled", true);'
 REM_LINE = 'user_pref("devtools.debugger.remote-enabled", true);'
 WBS_LINE = 'user_pref("devtools.debugger.remote-websocket", true);'
@@ -21,6 +30,24 @@ THM_LINE = 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", tru
 UBI_LINE = 'user_pref("app.update.background.interval", -1);'
 UPD_LINE = 'user_pref("app.update.interval", -1);'
 URL_LINE = 'user_pref("browser.safebrowsing.provider.google.updateURL", "lolo");'
+
+RTC_T = 'user_pref("privacy.webrtc.hideGlobalIndicator", true);' -- after
+RTC_F = 'user_pref%(%"privacy.webrtc.hideGlobalIndicator%", false%);' -- before
+
+NAV_T = 'user_pref("media.navigator.permission.disabled", true);' -- after
+NAV_F = 'user_pref%(%"media.navigator.permission.disabled%", false%);' -- before
+
+PLY_T = 'user_pref%(%"media.block%-autoplay%-until%-in%-foreground%", true%);' -- before
+PLY_F = 'user_pref("media.block-autoplay-until-in-foreground", false);' -- after
+
+RST_A = 'user_pref("extensions.webextensions.restrictedDomains", "");' -- after
+RST_B = 'user_pref%(%"extensions.webextensions.restrictedDomains%", %".*%"%);' -- before
+
+FNG_T = 'user_pref%(%"privacy.resistFingerprinting.block_mozAddonManager%", true%);' -- before
+FNG_F = 'user_pref("privacy.resistFingerprinting.block_mozAddonManager", false);' -- after
+
+SGN_T = 'user_pref%(%"xpinstall.signatures.required%", true%);' -- before
+SGN_F = 'user_pref("xpinstall.signatures.required", false);' -- after
 
 if getFirstMatch(pref_content, CRM_PATTERN) == nil then
 	appendLine(pref_path, CRM_LINE)
@@ -44,6 +71,42 @@ end
 
 if getFirstMatch(pref_content, UPD_PATTERN) == nil then
 	appendLine(pref_path, UPD_LINE)
+end
+
+if getEasyMatch(pref_content, RTC_PATTERN) == nil then
+	appendLine(pref_path, RTC_T)
+else
+	replaceLine(pref_path, RTC_F, RTC_T)
+end
+
+if getEasyMatch(pref_content, NAV_PATTERN) == nil then
+	appendLine(pref_path, NAV_T)
+else
+	replaceLine(pref_path, NAV_F, NAV_T)
+end
+
+if getEasyMatch(pref_content, PLY_PATTERN) == nil then
+	appendLine(pref_path, PLY_F)
+else
+	replaceLine(pref_path, PLY_T, PLY_F)
+end
+
+if getEasyMatch(pref_content, RST_PATTERN) == nil then
+	appendLine(pref_path, RST_A)
+else
+	replaceLine(pref_path, RST_B, RST_A)
+end
+
+if getEasyMatch(pref_content, FNG_PATTERN) == nil then
+	appendLine(pref_path, FNG_F)
+else
+	replaceLine(pref_path, FNG_T, FNG_F)
+end
+
+if getEasyMatch(pref_content, SGN_PATTERN) == nil then
+	appendLine(pref_path, SGN_F)
+else
+	replaceLine(pref_path, SGN_T, SGN_F)
 end
 
 print(osName())
