6bb602427dea16a46d5d72f167aeb5ce8b899245
bijanbina <bijanbina@gmail.com>
2024-02-28 08:42:08 +0100

    Rebound: Transfer to Timer Mode for GMeet and Confluence

--- Rebound/Scripts/Js/confluence.js
+++ Rebound/Scripts/Js/confluence.js
@@ -1,54 +1,40 @@
-var is_join = 0;
-var is_min = 0;
-var is_present = 0;
-
+var conf_is_join = 0;
 function main_conf()
 {
-    var is_join = 0;
-    var is_min = 0;
-    var is_present = 0;
+    join_timer = setInterval(conf_timeoutMain, 500);
+} 
 
-    sleep(2000);
+function shortcut_filter()
+{
+    return $(this).html() === "Shortcuts";
+}
 
-    const autoAdmit = (mutation) =>
+function conf_timeoutMain()
+{
+    var sh = $("div").filter(shortcut_filter);
+    var pr = sh.parent().parent().parent();
+    var apps = $('div[data-testid="space-apps"]');
+    var ss = $("span:contains('Space settings')");
+    var ss_pr = ss.parent().parent();
+    var at = $("span:contains('Automation')");
+    var at_pr = at.parent().parent();
+    var ac = $("span:contains('All content')");
+    var ac_pr = ac.parent().parent();
+    var ad = $("span:contains('You‘re on the Free plan')");
+    var ad_pr = ad.parent().parent();
+    var ip = $("span:contains('Invite people')");
+    var ip_pr = ip.parent().parent();
+
+    pr.remove();
+    ss_pr.remove();
+    at_pr.remove();
+    ac_pr.remove();
+    ad_pr.remove();
+    ip_pr.remove();
+    apps.remove();
+
+    if( ad.length )
     {
-        if (!mutation.addedNodes)
-        {
-            return;
-        }
-        //var sh = $("div:contains('Shortcuts')");
-        var sh = $("div").filter(shortcut_filter);
-        var pr = sh.parent().parent().parent();
-        var apps = $('div[data-testid="space-apps"]');
-        var ss = $("span:contains('Space settings')");
-        var ss_pr = ss.parent().parent();
-        var at = $("span:contains('Automation')");
-        var at_pr = at.parent().parent();
-        var ac = $("span:contains('All content')");
-        var ac_pr = ac.parent().parent();
-        var ad = $("span:contains('You‘re on the Free plan')");
-        var ad_pr = ad.parent().parent();
-
-        if( is_join==0 )
-        {
-            pr.remove();
-            ss_pr.remove();
-            at_pr.remove();
-            ac_pr.remove();
-            ad_pr.remove();
-            apps.remove();
-
-            if( ad.length )
-            {
-                is_join = 1;
-            }
-        }
-    };
-
-    const observer = new MutationObserver((mutations) => mutations.forEach(autoAdmit));
-
-    // Options for the observer (which mutations to observe)
-    const obs_config = { attributes: true, characterData: false, childList: true, subtree: true };
-
-    //observer.observe(document.body, obs_config);
+        clearInterval(gacc_timer);
+    }
 }
--- Rebound/Scripts/Js/gmeet.js
+++ Rebound/Scripts/Js/gmeet.js
@@ -1,47 +1,67 @@
 var join_timer;
+var gmeet_is_min = 0;
+var gmeet_is_present = 0;
+
 function main_gmeet()
 {
-    join_timer = setInterval(click_join, 100);
-    var is_min = 0;
-    var is_present = 0;
-
-    const autoAdmit = (mutation) =>
+    if( window.location.href.includes("authuser=1")==0 )
     {
-        if (!mutation.addedNodes)
-            return
+        join_timer = setInterval(gmeet_timeoutMain, 500);
+    }
+}
 
-        if( is_min==0 )
+async function gmeet_timeoutMain()
+{
+    //if( is_min==0 )
+    //{
+    //    var min_list = document.evaluate("//div[text()='Minimize']", document, null, XPathResult.ANY_TYPE, null );
+    //    var min_div = min_list.iterateNext();
+
+    //    var min_btn = min_div.parentElement.firstChild;
+
+    //    if( min_btn!=null )
+    //    {
+    //        min_btn.click();
+    //        is_min = 1;
+    //    }
+    //}
+
+    //if( is_present==0 )
+    //{
+    //    /// Is Presenting
+    //    var present_list = document.evaluate("//span[contains(., 'is presenting')]", document, null, XPathResult.ANY_TYPE, null );
+    //    var pre_span = present_list.iterateNext();
+
+    //    if( pre_span!=null )
+    //    {
+    //        pre_span.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+    //        is_present = 1;
+    //    }
+    //}
+
+    var j_node;
+    if( $("span:contains('Ask to join')").length==0 )
+    {
+        if( $("span:contains('Join now')").length==0 )
         {
-            var min_list = document.evaluate("//div[text()='Minimize']", document, null, XPathResult.ANY_TYPE, null );
-            var min_div = min_list.iterateNext();
-
-            var min_btn = min_div.parentElement.firstChild;
-
-            if( min_btn!=null )
-            {
-                min_btn.click();
-                is_min = 1;
-            }
+            return;
         }
-
-        if( is_present==0 )
+        else
         {
-            /// Is Presenting
-            var present_list = document.evaluate("//span[contains(., 'is presenting')]", document, null, XPathResult.ANY_TYPE, null );
-            var pre_span = present_list.iterateNext();
-
-            if( pre_span!=null )
-            {
-                pre_span.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
-                is_present = 1;
-            }
+            j_node = $("span:contains('Join now')");
         }
+    }
+    else
+    {
+        j_node = $("span:contains('Ask to join')");
+    }
 
-        const observer = new MutationObserver((mutations) => mutations.forEach(autoAdmit));
-
-        // Options for the observer (which mutations to observe)
-        const obs_config = { attributes: true, characterData: false, childList: true, subtree: true };
-
-        observer.observe(document.body, obs_config);
+    if( $('[data-tooltip="Turn off camera (ctrl + e)"]').length==1 )
+    {
+        await sleep(500);
+        $('[data-tooltip="Turn off camera (ctrl + e)"]').click();
+        await sleep(800);
+        j_node.click();
+        clearInterval(join_timer);
     }
 }
